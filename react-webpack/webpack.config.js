98038a31208a949168db53240d0346465260a10a
marcio lira <machollanda@gmail.com>
2020-09-07 16:13:38 -0300

    adicionado webpackage

---
+++ react-webpack/webpack.config.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const path = require('path')
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+}
+
+
+
+
